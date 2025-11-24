52d012ae5aed1dff090573de2297a007874ac224
Tom <243240489+TomsToybox@users.noreply.github.com>
2025-11-24 06:11:15 -0500

    debug

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -15,7 +15,7 @@ cd Build
 
 CompilerOptions="-o main"  # Output file will be named "main"
 CompilerOptions+=" -Wall  -Werror "  # Turn on more compiler warnings and treat them as errors
-CompilerOptions+=" -Weverything" # Warn all of the things!
+CompilerOptions+=" -Wextra" # Warn all of the things!
 CompilerOptions+=" -Wno-c++98-compat" # Don't need C++98 comptability
 CompilerOptions+=" -Wno-padded" # Don't care about padding
 CompilerOptions+=" -Wno-global-constructors" # We need global ctors
